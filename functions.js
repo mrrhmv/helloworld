68c461b36b825d5277fd4ebf1f60a9195c63a6e6
mr <rahimmr011>
2019-08-09 08:46:18 -0700

    'first'

---
+++ functions.js
@@ -0,0 +1,46 @@
+const getSavedTodos = function(){
+    if (todosJSON !== null) {
+        todos = JSON.parse(todosJSON)
+    }
+}   
+
+const saveTodos = function(){
+ localStorage.getItem('todos')
+}
+
+const renderTodos = function (todos, filters) {
+    const filteredTodos = todos.filter(function (todo) {
+        const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+        const hideCompletedMatch = !filters.hideCompleted || !todo.completed
+
+        return searchTextMatch && hideCompletedMatch
+    })
+
+    const incompleteTodos = filteredTodos.filter(function (todo) {
+        return !todo.completed
+    })
+
+    document.querySelector('#todos').innerHTML = ''
+
+    const summary = document.createElement('h2')
+    summary.textContent = `You have ${incompleteTodos.length} todos left`
+    document.querySelector('#todos').appendChild(summary)
+
+    filteredTodos.forEach(function (todo) {
+        const p = document.createElement('p')
+        p.textContent = todo.text
+        document.querySelector('#todos').appendChild(p)
+    })
+}
+const generateTodoDom = function(){
+document.querySelector('#new-todo').addEventListener('submit', function (e) {
+    e.preventDefault()
+    todos.push({
+        text: e.target.elements.text.value,
+        completed: false
+    })
+    localStorage.setItem('todos', JSON.stringify(todos))
+    renderTodos(todos, filters)
+    e.target.elements.text.value = ''
+})
+}
